c840473174008e23c163fc7c1b5af174858578e6
MorIsUmmeryuAn <140309485+MorIsUmmeryuAn@users.noreply.github.com>
2025-01-02 16:05:06 +0800

    Add files via upload

--- example/callee/friendservice.cc
+++ example/callee/friendservice.cc
@@ -4,6 +4,7 @@
 #include "mprpcapplication.h"
 #include "rpcprovider.h"
 #include<vector>
+#include"logger.h"
 
 class FriendService:public fixbug::FriendServiceRpc
 {
@@ -34,6 +35,10 @@ public:
 
 
 int main(int argc,char **argv){
+
+    LOG_INFO("first log message!");
+    LOG_ERROR("%s:%s:%d",__FILE__,__FUNCTION__,__LINE__);
+
     //调用框架的初始化操作
     MprpcApplication::Init(argc,argv);
     
